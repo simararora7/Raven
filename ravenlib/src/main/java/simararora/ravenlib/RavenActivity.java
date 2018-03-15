8b96235c490fcb7fc0e806f6daf5d99e8bf02919
simarpreetsingharora <simarpreetsingharora@gmail.com>
2018-03-16 01:28:56 +0530

    Singl task

--- ravenlib/src/main/java/simararora/ravenlib/RavenActivity.java
+++ ravenlib/src/main/java/simararora/ravenlib/RavenActivity.java
@@ -1,5 +1,6 @@
 package simararora.ravenlib;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
@@ -15,6 +16,17 @@ public abstract class RavenActivity extends AppCompatActivity implements ParseCo
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        initRaven();
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+        setIntent(intent);
+        initRaven();
+    }
+
+    private void initRaven(){
         Raven.getInstance().parse(getIntent(), this);
     }
 }
